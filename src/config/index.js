d4b6c4c462d57d72a12362984d2d8facaf5ffa68
code-rmc <rodrigoslmatias@gmail.com>
2022-01-20 19:41:14 -0300

    add config DB

--- src/config/index.js
+++ src/config/index.js
@@ -17,4 +17,9 @@ module.exports = {
     secret: process.env.SECRET,
     ttl: process.env.TTL,
   },
+  db: {
+    name: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const ServerExpress = require("./loader/server");
-
+const conectionTest = require("./loader/db");
 const start = async () => {
   console.log("*******************");
   console.log("LOADING.....");
+  await conectionTest();
   const server = new ServerExpress();
   await server.start();
   console.log("SERVER STARTED..");
